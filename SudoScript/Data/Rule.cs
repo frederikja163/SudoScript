9eb298e1bbf35edad7429df4f46e905f262dbd66
FrederikJA <fjaplv64@gmail.com>
2023-03-28 14:37:56 +0200

    Add one-rule rule

--- SudoScript/Data/Rule.cs
+++ SudoScript/Data/Rule.cs
@@ -1,7 +1,60 @@
 ﻿namespace SudoScript.Data;
 
+public sealed class RuleException : Exception
+{
+    public RuleException(Unit unit, Cell cell, string message) : base(message)
+    {
+        Unit = unit;
+        Cell = cell;
+    }
+
+    public Unit Unit { get; }
+    public Cell Cell { get; }
+}
+
 public interface IRule
 {
     public abstract void EliminateCandidates(Unit unit);
     public abstract bool ValidateRules(Unit unit);
+}
+
+public sealed class OneRule : IRule
+{
+    public void EliminateCandidates(Unit unit)
+    {
+        HashSet<int> seenDigits = new HashSet<int>();
+        foreach (Cell cell in unit.Cells())
+        {
+            if (cell.Digit == 0)
+            {
+                // Remove all seen digits from the candidates of this cell.
+                cell.Candidates.RemoveWhere(x => seenDigits.Contains(x));
+            }
+            else
+            {
+                if (seenDigits.Contains(cell.Digit))
+                {
+                    throw new RuleException(unit, cell, $"One rule is not followed, multiple instances of {cell.Digit} found.");
+                }
+                seenDigits.Add(cell.Digit);
+            }
+        }
+    }
+
+    public bool ValidateRules(Unit unit)
+    {
+        HashSet<int> seenDigits = new HashSet<int>();
+        foreach (Cell cell in unit.Cells())
+        {
+            if (cell.Digit != 0)
+            {
+                if (seenDigits.Contains(cell.Digit))
+                {
+                    return false;
+                }
+                seenDigits.Add(cell.Digit);
+            }
+        }
+        return true;
+    }
 }
