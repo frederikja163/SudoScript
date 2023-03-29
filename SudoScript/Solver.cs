9584b97dfd852e273799daadcc065a363279b021
FrederikJA <fjaplv64@gmail.com>
2023-03-29 11:56:27 +0200

    Test code for solver

--- SudoScript/Solver.cs
+++ SudoScript/Solver.cs
@@ -1,4 +1,5 @@
 ﻿using SudoScript.Data;
+using System.Diagnostics.CodeAnalysis;
 
 namespace SudoScript;
 
@@ -8,6 +9,27 @@ public static class Solver
     // Perhaps a multithreading solver instead of actual back tracking to calculate multiple paths at once.
     public static Board Solve(Board board)
     {
+        if (!SolveRec(board, out Board? newBoard))
+        {
+            throw new Exception("Board is not solveable");
+        }
+
+        return newBoard;
+    }
+
+    private static bool SolveRec(Board board, [NotNullWhen(true)] out Board? solvedBoard)
+    {
+        // Get a list of all cells with the least amount of candidates.
+
+        // Choose one of the cells.
+
+        // Create a clone of the board for backtracking.
+
+        // Collapse the cell with a digit.
+
+        // Eliminate candidates from all rules.
+
+        // Call solve on the new board.
         throw new NotImplementedException();
     }
 
