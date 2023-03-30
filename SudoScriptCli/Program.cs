68758cf23b73acb2be8eab63cc5391912628ea19
FrederikJA <fjaplv64@gmail.com>
2023-03-30 13:08:43 +0200

    Remove test code

--- SudoScriptCli/Program.cs
+++ SudoScriptCli/Program.cs
@@ -8,96 +8,16 @@ internal static class Program
 {
     internal static void Main(string[] args)
     {
-        // TODO: This is temporary, to test, develop and debug the solver.
-        List<IRule> oneRule = new List<IRule>() { new OneRule() };
-
-        List<Cell> allCells = new List<Cell>();
-        List<Unit> units = new List<Unit>();
-
-        for (int x = 1; x <= 9; x++)
+        if (args.Length == 0)
         {
-            for (int y = 1; y <= 9; y++)
-            {
-                allCells.Add(new Cell(x, y));
-            }
+            Console.WriteLine("No arguments provided, try with 'dotnet run SudoScriptCli [SudoScriptFile]'");
+            return;
         }
 
-        units.Add(CreateRow(1));
-        units.Add(CreateRow(2));
-        units.Add(CreateRow(3));
-        units.Add(CreateRow(4));
-        units.Add(CreateRow(5));
-        units.Add(CreateRow(6));
-        units.Add(CreateRow(7));
-        units.Add(CreateRow(8));
-        units.Add(CreateRow(9));
-
-        units.Add(CreateColumn(1));
-        units.Add(CreateColumn(2));
-        units.Add(CreateColumn(3));
-        units.Add(CreateColumn(4));
-        units.Add(CreateColumn(5));
-        units.Add(CreateColumn(6));
-        units.Add(CreateColumn(7));
-        units.Add(CreateColumn(8));
-        units.Add(CreateColumn(9));
-
-        units.Add(CreateBox(1, 1));
-        units.Add(CreateBox(1, 4));
-        units.Add(CreateBox(1, 7));
-        units.Add(CreateBox(4, 1));
-        units.Add(CreateBox(4, 4));
-        units.Add(CreateBox(4, 7));
-        units.Add(CreateBox(7, 1));
-        units.Add(CreateBox(7, 4));
-        units.Add(CreateBox(7, 7));
-
-        Board board = new Board(allCells, units);
-        Solver.Solve(board);
-
-        Unit CreateRow(int y)
-        {
-            List<CellReference> cells = new List<CellReference> ();
-            for (int x = 1; x <= 9; x++)
-            {
-                cells.Add(new CellReference(x, y));
-            }
-            return new Unit(cells, oneRule);
-        }
-
-        Unit CreateColumn(int x)
-        {
-            List<CellReference> cells = new List<CellReference>();
-            for (int y = 1; y <= 9; y++)
-            {
-                cells.Add(new CellReference(x, y));
-            }
-            return new Unit(cells, oneRule);
-        }
-
-        Unit CreateBox(int x, int y)
-        {
-            List<CellReference> cells = new List<CellReference>();
-            for (int x1 = 0; x1 < 3; x1++)
-            {
-                for (int y1 = 0; y1 < 3; y1++)
-                {
-                    cells.Add(new CellReference(x + x1, y + y1));
-                }
-            }
-            return new Unit(cells, oneRule);
-        }
-
-        //if (args.Length == 0)
-        //{
-        //    Console.WriteLine("No arguments provided, try with 'dotnet run SudoScriptCli [SudoScriptFile]'");
-        //    return;
-        //}
-
-        //using StreamReader reader = new StreamReader(args[0]);
-        //ProgramNode programNode = Parser.ParseProgram(reader);
-        //Board board = Generator.GetBoardFromAST(programNode);
-        //Board solvedBoard = Solver.Solve(board);
+        using StreamReader reader = new StreamReader(args[0]);
+        ProgramNode programNode = Parser.ParseProgram(reader);
+        Board board = Generator.GetBoardFromAST(programNode);
+        Board solvedBoard = Solver.Solve(board);
 
         // Somehow visualize the board.
     }
