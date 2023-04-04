eb6e4fb8ed90a435307afac9e2db026435c54725
Toxic <markusbolsen@gmail.com>
2023-04-04 09:06:57 +0200

    Added simple stringifier for board.

--- SudoScript/Data/Board.cs
+++ SudoScript/Data/Board.cs
@@ -60,4 +60,48 @@ public sealed class Board : ICloneable
     {
         return Clone();
     }
+
+    public string Stringify() {
+
+        int minX = int.MaxValue;
+        int maxX = int.MinValue;
+        int minY = int.MaxValue;
+        int maxY = int.MinValue;
+
+        foreach(Cell cell in _cells.Values) {
+            if(cell.X > maxX) maxX = cell.X;
+            if(cell.X < minX) minX = cell.X;
+            if(cell.Y > maxY) maxY = cell.Y;
+            if(cell.Y < minY) minY = cell.Y;
+        }
+
+        List<Cell> orderedCellList = _cells.Values
+            .OrderBy(c => c.X)
+            .OrderBy(c => c.Y)
+            .ToList();
+
+        string s = "";
+
+        int i = 0;
+        int width = maxX - minX;
+        int height = maxY - minY;
+        for(int row = 0; row < height; row++) {
+            for(int col = 0; col < width; col++) {
+
+                string digitString = " ";
+                if(orderedCellList[i].X == col + minX && orderedCellList[i].Y == row + minY) {
+                    if(orderedCellList[i].Digit != Cell.EmptyDigit) {
+                        digitString = orderedCellList[i].Digit.ToString();
+                    }
+                    i++;
+                }
+
+                string space = col == width - 1 ? "\t" : "";
+                s += $"[{digitString}]" + space;
+            }
+            s += row == maxY - minY - 1 ? "\n\n" : "";
+        }
+
+        return s;
+    }
 }
