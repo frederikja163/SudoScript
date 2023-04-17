7ee8d7d8545e0ea1314232a24988ac54388769cd
Emilorzz <emil69h@gmail.com>
2023-04-17 14:53:15 +0200

    added Tests for ParseRule. Also solved some minor issues in the Parser.cs

--- Tests/ParserTests.cs
+++ Tests/ParserTests.cs
@@ -35,4 +35,40 @@ internal sealed class ParserTests
         Assert.That(tree.Child?.NameToken.Match, Is.EqualTo("UnitName"));
         Assert.That(tree.Child?.Parameters?.Count, Is.EqualTo(2));
     }
+
+    [Test]
+    public void ParseRuleTest() 
+    {
+        using StreamReader stream = new StreamReader("./TestData/EmptyRuleUnit.txt");
+        var tree = Parser.ParseProgram(stream);
+
+        Assert.That(tree.Child.UnitStatements.Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void ParseRuleTestWithFunctionNoArgs()
+    {
+        using StreamReader stream = new StreamReader("./TestData/RuleUnitWithFuncNoArgs.txt");
+        var tree = Parser.ParseProgram(stream);
+
+        Assert.That(tree.Child.UnitStatements[0].Children().Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void ParseRuleTestWithFunctionWithArgs()
+    {
+        using StreamReader stream = new StreamReader("./TestData/RuleUnitWithFuncWithArgs.txt");
+        var tree = Parser.ParseProgram(stream);
+
+        Assert.That(tree.Child.UnitStatements[0].Children().Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void ParseRuleTestWithFunctionMultipleArgs()
+    {
+        using StreamReader stream = new StreamReader("./TestData/RuleUnitWithFuncMultipleArgs.txt");
+        var tree = Parser.ParseProgram(stream);
+
+        Assert.That(tree.Child.UnitStatements[0].Children().Count, Is.EqualTo(2));
+    }
 }
