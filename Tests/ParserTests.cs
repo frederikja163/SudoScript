68dca36d6c82216003adda55db5dbd0ecee29bb5
Emilorzz <emil69h@gmail.com>
2023-04-17 13:49:41 +0200

    refactored tests for parsing units. also refactoring of said tests

--- Tests/ParserTests.cs
+++ Tests/ParserTests.cs
@@ -12,9 +12,8 @@ internal sealed class ParserTests
         using StreamReader stream = new StreamReader("./TestData/UnitWithoutName.txt");
         var tree = Parser.ParseProgram(stream);
 
-        UnitNode testNode = new UnitNode(null, new(), new());
-
-        Assert.That(tree.Child.Equals(testNode));
+        Assert.That(tree.Child?.NameToken, Is.EqualTo(null));
+        Assert.That(tree.Child?.Parameters?.Count, Is.EqualTo(0));
     }
 
     [Test]
@@ -23,10 +22,8 @@ internal sealed class ParserTests
         using StreamReader stream = new StreamReader("./TestData/UnitWithName.txt");
         var tree = Parser.ParseProgram(stream);
 
-        Token nameToken = new Token(TokenType.Identifier, "UnitName", "unit UnitName {\n", 1, 6, "UnitWithName.txt");
-        UnitNode testNode = new UnitNode(nameToken, new(), new());
-
-        Assert.That(tree.Child.Equals(testNode));
+        Assert.That(tree.Child?.NameToken.Match, Is.EqualTo("UnitName"));
+        Assert.That(tree.Child?.Parameters?.Count, Is.EqualTo(0));
     }
 
     [Test]
@@ -35,17 +32,7 @@ internal sealed class ParserTests
         using StreamReader stream = new StreamReader("./TestData/UnitWithParams.txt");
         var tree = Parser.ParseProgram(stream);
 
-        Token nameToken = new Token(TokenType.Identifier, "UnitName", "unit UnitName {\n", 1, 6, "UnitWithParams.txt");
-        List<ParameterNode> children = new List<ParameterNode>();
-
-        children.Add(new ParameterIdentifierNode(new Token(TokenType.Identifier, "a", "unit UnitName a (x,y) {\n", 1, 15, "UnitWithParams.txt")));
-
-        ParameterIdentifierNode x = new ParameterIdentifierNode(new Token(TokenType.Identifier, "x", "unit UnitName a (x,y) {\n", 1, 18, "UnitWithParams.txt"));
-        ParameterIdentifierNode y = new ParameterIdentifierNode(new Token(TokenType.Identifier, "y", "unit UnitName a (x,y) {\n", 1, 20, "UnitWithParams.txt"));
-        children.Add(new ParameterCellNode(x,y));
-
-        UnitNode testNode = new UnitNode(nameToken, new(), children);
-
-        Assert.That(tree.Child.Equals(testNode));
+        Assert.That(tree.Child?.NameToken.Match, Is.EqualTo("UnitName"));
+        Assert.That(tree.Child?.Parameters?.Count, Is.EqualTo(2));
     }
 }
