6601caa9515db7d7f7e17938d4c01959c2d8ada5
IronicJuice <81492870+IronicJuice@users.noreply.github.com>
2023-04-19 20:37:40 +0200

    Finished row implementation. No tests

--- StandardLibrary/Row.cs
+++ StandardLibrary/Row.cs
@@ -9,38 +9,21 @@ namespace StandardLibrary
 {
     internal class Row : Unit
     {
-        public Row(int a) : base(InitCells(a), new IRule[] { new OneRule() }) //X automatically becomes 1
+        public Row(int y) : base(InitCells(1, y), new IRule[] { new OneRule() }) //X is assumed to be 1
         {
-            
+        }
+        public Row(int x, int y) : base(InitCells(x, y), new IRule[] { new OneRule() })
+        {
         }
 
-        private static IReadOnlyList<CellReference> InitCells(int a)
+        private static IReadOnlyList<CellReference> InitCells(int x, int y)
         {
             List<CellReference> rowCells = new List<CellReference>();
-            for (int i = 0; i < 9; i++)
+            for (int i = x; i <= i + 9; i++)
             {
-                rowCells.Add(new CellReference(cells[0].X, cells[0].Y));
+                rowCells.Add(new CellReference(i, y));
             }
             return rowCells.ToArray();
-
-        }
-
-        private bool ValidateRow(IReadOnlyList<CellReference> cells)
-        {
-            if (cells == null) { return false; }
-            if (cells.Count != 9) { return false; }
-            for (int i = 0; i < cells.Count-1; i++) //We do not want to check the last cell since it has already been compared to the previous cell
-            {
-                if (cells[i].X+1 != cells[i + 1].X) //Test if the cells are adjacent
-                {
-                    return false;
-                }
-                if (cells[i].Y != cells[i + 1].Y)
-                {
-                    return false;
-                }
-            }
-            return true;
         }
     }
 }
