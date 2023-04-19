88f9ed53a973b28f938c6082da3a2b67295b144f
IronicJuice <81492870+IronicJuice@users.noreply.github.com>
2023-04-19 13:18:17 +0200

    Fixed comparison on line 69 in Sum.cs

--- StandardLibrary/Sum.cs
+++ StandardLibrary/Sum.cs
@@ -66,7 +66,7 @@ namespace StandardLibrary
             {
                 foreach (int candidate in currentCell.Candidates())
                 {
-                    if (!(runningSum + candidate > remainder))
+                    if (!(runningSum + candidate => remainder))
                     {
                         return true;
                     }
