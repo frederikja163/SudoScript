d9d71ef5aff0ee3ec8dbf0b58bade47acf723ee1
IronicJuice <81492870+IronicJuice@users.noreply.github.com>
2023-04-20 17:13:04 +0200

    Finished implementation and started writing tests

---
+++ StandardLibrary.Tests/OddTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using SudoScript.Data;
+using Tests;
+
+namespace StandardLibrary.Tests
+{
+    internal sealed class OddTests
+    {
+        [Test]
+        public void ValidOddUnitTest()
+        {
+            Unit unit = new Unit(new List<CellReference>
+            {
+                (1, 1),
+                (2, 2),
+                (3, 3),
+                (4, 4),
+                (5, 5),
+                (6, 6),
+                (7, 7),
+            }, new List<IRule>
+            {
+                new Odd()
+            });
+            Board board = Util.CreateStandardEmpty(unit);
+            board[1, 1].Digit = 9;
+            board[2, 2].Digit = 7;
+            board[3, 3].Digit = 5;
+            Assert.IsTrue(unit.ValidateRules());
+        }
+    }
+}
