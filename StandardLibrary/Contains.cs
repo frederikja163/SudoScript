8b1561b5864de73c25e2e711582486b376c7e886
IronicJuice <81492870+IronicJuice@users.noreply.github.com>
2023-04-23 07:59:15 +0200

    Started implementing contains rule

---
+++ StandardLibrary/Contains.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SudoScript.Data;
+
+namespace StandardLibrary
+{
+    public class Contains : IRule
+    {
+        public bool EliminateCandidates(Unit unit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidateRules(Unit unit, int number)
+        {
+            bool contains = false;
+        }
+    }
+}
