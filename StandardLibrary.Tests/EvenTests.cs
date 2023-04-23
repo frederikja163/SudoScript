bb5b7dd4d3e666902a22b2218bef641d8b0effd0
IronicJuice <81492870+IronicJuice@users.noreply.github.com>
2023-04-23 08:36:49 +0200

    Created EliminateCandidatesTest() for odd and even

--- StandardLibrary.Tests/EvenTests.cs
+++ StandardLibrary.Tests/EvenTests.cs
@@ -52,5 +52,34 @@ namespace StandardLibrary.Tests
             board[4, 4].Digit = 2;
             Assert.IsFalse(unit.ValidateRules());
         }
+        [Test]
+        public void EliminateCandidatesTest() 
+        {
+            Unit unit = new Unit(new List<CellReference>
+            {
+                (1, 1),
+                (2, 2),
+                (3, 3),
+                (4, 4),
+                (5, 5),
+                (6, 6),
+                (7, 7),
+            }, new List<IRule>
+            {
+                new Even()
+            });
+            Board board = Util.CreateStandardEmpty(unit);
+            unit.EliminateCandidates();
+
+            foreach (Cell cell in unit.Cells())
+            {
+                Assert.That(cell.Candidates(), Is.EqualTo(new List<int>{ 2, 4, 6, 8}));
+                Assert.IsFalse(cell.HasCandidate(1));
+                Assert.IsFalse(cell.HasCandidate(3));
+                Assert.IsFalse(cell.HasCandidate(5));
+                Assert.IsFalse(cell.HasCandidate(7));
+                Assert.IsFalse(cell.HasCandidate(9));
+            }
+        }
     }
 }
--- StandardLibrary.Tests/OddTests.cs
+++ StandardLibrary.Tests/OddTests.cs
@@ -52,5 +52,33 @@ namespace StandardLibrary.Tests
             board[4, 4].Digit = 1;
             Assert.IsFalse(unit.ValidateRules());
         }
+        [Test]
+        public void EliminateCandidatesTest()
+        {
+            Unit unit = new Unit(new List<CellReference>
+            {
+                (1, 1),
+                (2, 2),
+                (3, 3),
+                (4, 4),
+                (5, 5),
+                (6, 6),
+                (7, 7),
+            }, new List<IRule>
+            {
+                new Odd()
+            });
+            Board board = Util.CreateStandardEmpty(unit);
+            unit.EliminateCandidates();
+
+            foreach (Cell cell in unit.Cells())
+            {
+                Assert.That(cell.Candidates(), Is.EqualTo(new List<int> { 1, 3, 5, 7, 9 }));
+                Assert.IsFalse(cell.HasCandidate(2));
+                Assert.IsFalse(cell.HasCandidate(4));
+                Assert.IsFalse(cell.HasCandidate(6));
+                Assert.IsFalse(cell.HasCandidate(8));
+            }
+        }
     }
 }
