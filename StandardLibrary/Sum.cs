636c2fb59cc7e40990cf0851c296f707373d00be
FrederikJA <fjaplv64@gmail.com>
2023-05-04 08:49:23 +0200

    Revert sum changes

--- StandardLibrary/Sum.cs
+++ StandardLibrary/Sum.cs
@@ -87,18 +87,9 @@ namespace StandardLibrary
         public bool ValidateRules(Unit unit)
         {
             int counter = 0;
-            bool isAnyEmpty = false;
-            foreach (Cell cell in unit.Cells())
-            {
-                if (cell.Digit == Cell.EmptyDigit)
-                {
-                    isAnyEmpty = true;
-                }
-                counter += cell.Digit;
-            }
-
-            return (counter == SumVal && !isAnyEmpty) ||
-                   (counter < SumVal && isAnyEmpty);
+            foreach (Cell cell in unit.Cells()) { counter += cell.Digit; }
+            if (counter == SumVal) { return true; }
+            return false;
         }
     }
 }
