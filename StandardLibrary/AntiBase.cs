b6a58e1bbd959bbbe88e72617fa078cc9ba46d80
IronicJuice <81492870+IronicJuice@users.noreply.github.com>
2023-05-10 08:47:51 +0200

    Created abstract class

---
+++ StandardLibrary/AntiBase.cs
@@ -0,0 +1,22 @@
+﻿using SudoScript.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StandardLibrary
+{
+    public abstract class AntiBase : IRule
+    {
+        public bool EliminateCandidates(Unit unit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidateRules(Unit unit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
