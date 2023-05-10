ef9248ae13f09de6e9ceaa4c680298e94291f023
FrederikJA <fjaplv64@gmail.com>
2023-05-10 10:09:00 +0200

    Fix namespaces

--- StandardLibrary.Test/ContainsTests.cs
+++ StandardLibrary.Test/ContainsTests.cs
@@ -1,14 +1,14 @@
 ﻿using NUnit.Framework;
 using SudoScript.Core.Data;
 
-namespace StandardLibrary.Test
+namespace StandardLibrary.Test;
+
+public sealed class ContainsTests
 {
-    public sealed class ContainsTests
+    [Test]
+    public void ValidContainsUnitTest()
     {
-        [Test]
-        public void ValidContainsUnitTest()
-        {
-            Unit unit = new Unit(new List<CellReference>
+        Unit unit = new Unit(new List<CellReference>
         {
             (1, 1),
             (2, 2),
@@ -22,49 +22,21 @@ namespace StandardLibrary.Test
             new Contains(9)
         }) ;
 
-            Board board = Util.CreateStandardEmpty(unit);
-            board[1, 1].Digit = 1;
-            board[2, 2].Digit = 2;
-            board[4, 4].Digit = 4;
-            board[5, 5].Digit = 9;
-            board[7, 7].Digit = 7;
+        Board board = Util.CreateStandardEmpty(unit);
+        board[1, 1].Digit = 1;
+        board[2, 2].Digit = 2;
+        board[4, 4].Digit = 4;
+        board[5, 5].Digit = 9;
+        board[7, 7].Digit = 7;
 
-            Assert.IsTrue(unit.ValidateRules());
-            Assert.DoesNotThrow(() => unit.EliminateCandidates());
-        }
+        Assert.IsTrue(unit.ValidateRules());
+        Assert.DoesNotThrow(() => unit.EliminateCandidates());
+    }
 
-        [Test]
-        public void InvalidContainsUnitTest()
-        {
-            Unit unit = new Unit(new List<CellReference>
-        {
-            (1, 1),
-            (2, 2),
-            (3, 3),
-            (4, 4),
-            (5, 5),
-            (6, 6),
-            (7, 7),
-        }, new List<IRule>
-        {
-            new Contains(8)
-        });
-
-            Board board = Util.CreateStandardEmpty(unit);
-            board[1, 1].Digit = 1;
-            board[2, 2].Digit = 2;
-            board[4, 4].Digit = 4;
-            board[5, 5].Digit = 9;
-            board[7, 7].Digit = 7;
-
-            Assert.IsFalse(unit.ValidateRules());
-            Assert.DoesNotThrow(() => unit.EliminateCandidates());
-        }
-
-        [Test]
-        public void EliminateCandidatesTest()
-        {
-            Unit unit = new Unit(new List<CellReference>
+    [Test]
+    public void InvalidContainsUnitTest()
+    {
+        Unit unit = new Unit(new List<CellReference>
         {
             (1, 1),
             (2, 2),
@@ -78,23 +50,50 @@ namespace StandardLibrary.Test
             new Contains(8)
         });
 
-            Board board = Util.CreateStandardEmpty(unit);
-            board[1, 1].Digit = 1;
-            board[2, 2].Digit = 2;
-            board[4, 4].Digit = 4;
-            board[5, 5].Digit = 9;
-            board[7, 7].Digit = 7;
+        Board board = Util.CreateStandardEmpty(unit);
+        board[1, 1].Digit = 1;
+        board[2, 2].Digit = 2;
+        board[4, 4].Digit = 4;
+        board[5, 5].Digit = 9;
+        board[7, 7].Digit = 7;
+
+        Assert.IsFalse(unit.ValidateRules());
+        Assert.DoesNotThrow(() => unit.EliminateCandidates());
+    }
+
+    [Test]
+    public void EliminateCandidatesTest()
+    {
+        Unit unit = new Unit(new List<CellReference>
+        {
+            (1, 1),
+            (2, 2),
+            (3, 3),
+            (4, 4),
+            (5, 5),
+            (6, 6),
+            (7, 7),
+        }, new List<IRule>
+        {
+            new Contains(8)
+        });
+
+        Board board = Util.CreateStandardEmpty(unit);
+        board[1, 1].Digit = 1;
+        board[2, 2].Digit = 2;
+        board[4, 4].Digit = 4;
+        board[5, 5].Digit = 9;
+        board[7, 7].Digit = 7;
             
-            Assert.IsFalse(unit.ValidateRules());
-            unit.EliminateCandidates();
-            Assert.IsTrue(board[3, 3].Candidates().Contains(8));
-            Assert.IsTrue(board[6, 6].Candidates().Contains(8));
+        Assert.IsFalse(unit.ValidateRules());
+        unit.EliminateCandidates();
+        Assert.IsTrue(board[3, 3].Candidates().Contains(8));
+        Assert.IsTrue(board[6, 6].Candidates().Contains(8));
 
-            board[3, 3].EliminateCandidate(8);
-            unit.EliminateCandidates();
+        board[3, 3].EliminateCandidate(8);
+        unit.EliminateCandidates();
 
-            Assert.IsTrue(board[6, 6].CandidateCount == 1);
-            Assert.IsTrue(board[6, 6].Candidates().Contains(8));
-        }
+        Assert.IsTrue(board[6, 6].CandidateCount == 1);
+        Assert.IsTrue(board[6, 6].Candidates().Contains(8));
     }
 }
--- StandardLibrary.Test/EvenTests.cs
+++ StandardLibrary.Test/EvenTests.cs
@@ -1,84 +1,83 @@
 ﻿using NUnit.Framework;
 using SudoScript.Core.Data;
 
-namespace StandardLibrary.Test
-{
-    internal sealed class EvenTests
-    {
-        [Test]
-        public void ValidEvenUnitTest()
-        {
-            Unit unit = new Unit(new List<CellReference>
-            {
-                (1, 1),
-                (2, 2),
-                (3, 3),
-                (4, 4),
-                (5, 5),
-                (6, 6),
-                (7, 7),
-            }, new List<IRule>
-            {
-                new Even()
-            });
-            Board board = Util.CreateStandardEmpty(unit);
-            board[1, 1].Digit = 8;
-            board[2, 2].Digit = 4;
-            board[3, 3].Digit = 4;
-            board[4, 4].Digit = 2;
-            Assert.IsTrue(unit.ValidateRules());
-        }
-        [Test]
-        public void InvalidEvenUnitTest()
-        {
-            Unit unit = new Unit(new List<CellReference>
-            {
-                (1, 1),
-                (2, 2),
-                (3, 3),
-                (4, 4),
-                (5, 5),
-                (6, 6),
-                (7, 7),
-            }, new List<IRule>
-            {
-                new Even()
-            });
-            Board board = Util.CreateStandardEmpty(unit);
-            board[1, 1].Digit = 9;
-            board[2, 2].Digit = 4;
-            board[3, 3].Digit = 4;
-            board[4, 4].Digit = 2;
-            Assert.IsFalse(unit.ValidateRules());
-        }
-        [Test]
-        public void EliminateCandidatesTest() 
-        {
-            Unit unit = new Unit(new List<CellReference>
-            {
-                (1, 1),
-                (2, 2),
-                (3, 3),
-                (4, 4),
-                (5, 5),
-                (6, 6),
-                (7, 7),
-            }, new List<IRule>
-            {
-                new Even()
-            });
-            Board board = Util.CreateStandardEmpty(unit);
-            unit.EliminateCandidates();
+namespace StandardLibrary.Test;
 
-            foreach (Cell cell in unit.Cells())
-            {
-                Assert.That(cell.Candidates(), Is.EqualTo(new List<int>{ 2, 4, 6, 8}));
-                Assert.IsFalse(cell.HasCandidate(1));
-                Assert.IsFalse(cell.HasCandidate(3));
-                Assert.IsFalse(cell.HasCandidate(5));
-                Assert.IsFalse(cell.HasCandidate(7));
-                Assert.IsFalse(cell.HasCandidate(9));
-            }
+internal sealed class EvenTests
+{
+    [Test]
+    public void ValidEvenUnitTest()
+    {
+        Unit unit = new Unit(new List<CellReference>
+        {
+            (1, 1),
+            (2, 2),
+            (3, 3),
+            (4, 4),
+            (5, 5),
+            (6, 6),
+            (7, 7),
+        }, new List<IRule>
+        {
+            new Even()
+        });
+        Board board = Util.CreateStandardEmpty(unit);
+        board[1, 1].Digit = 8;
+        board[2, 2].Digit = 4;
+        board[3, 3].Digit = 4;
+        board[4, 4].Digit = 2;
+        Assert.IsTrue(unit.ValidateRules());
+    }
+    [Test]
+    public void InvalidEvenUnitTest()
+    {
+        Unit unit = new Unit(new List<CellReference>
+        {
+            (1, 1),
+            (2, 2),
+            (3, 3),
+            (4, 4),
+            (5, 5),
+            (6, 6),
+            (7, 7),
+        }, new List<IRule>
+        {
+            new Even()
+        });
+        Board board = Util.CreateStandardEmpty(unit);
+        board[1, 1].Digit = 9;
+        board[2, 2].Digit = 4;
+        board[3, 3].Digit = 4;
+        board[4, 4].Digit = 2;
+        Assert.IsFalse(unit.ValidateRules());
+    }
+    [Test]
+    public void EliminateCandidatesTest() 
+    {
+        Unit unit = new Unit(new List<CellReference>
+        {
+            (1, 1),
+            (2, 2),
+            (3, 3),
+            (4, 4),
+            (5, 5),
+            (6, 6),
+            (7, 7),
+        }, new List<IRule>
+        {
+            new Even()
+        });
+        Board board = Util.CreateStandardEmpty(unit);
+        unit.EliminateCandidates();
+
+        foreach (Cell cell in unit.Cells())
+        {
+            Assert.That(cell.Candidates(), Is.EqualTo(new List<int>{ 2, 4, 6, 8}));
+            Assert.IsFalse(cell.HasCandidate(1));
+            Assert.IsFalse(cell.HasCandidate(3));
+            Assert.IsFalse(cell.HasCandidate(5));
+            Assert.IsFalse(cell.HasCandidate(7));
+            Assert.IsFalse(cell.HasCandidate(9));
         }
     }
 }
